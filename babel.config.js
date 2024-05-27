ad561c4ac228c47a4878e0364de86c8d6ee5b46b
Charlie L <charliesbot@gmail.com>
2024-05-26 19:55:37 -0700

    remove tamagui

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,7 @@
 module.exports = function (api) {
-  api.cache(true)
+  api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "@tamagui/babel-plugin",
-        {
-          components: ["tamagui"],
-          config: "./tamagui.config.ts",
-          logTimings: true,
-          disableExtraction: process.env.NODE_ENV === "development",
-        },
-      ],
-      // NOTE: this is only necessary if you are using reanimated for animations
-      "react-native-reanimated/plugin",
-    ],
-  }
-}
+    plugins: [],
+  };
+};
