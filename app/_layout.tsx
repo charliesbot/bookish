0116505cc6781328d0bdaa3d722b840fa376e5f5
Charlie L <charliesbot@gmail.com>
2024-05-26 18:32:12 -0700

    setup tamagui

---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+import "../tamagui-web.css";
+
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import { TamaguiProvider } from "tamagui";
+
+import { tamaguiConfig } from "../tamagui.config";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    // add this
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
+  );
+}
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,18 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === "development",
+        },
+      ],
+      // NOTE: this is only necessary if you are using reanimated for animations
+      "react-native-reanimated/plugin",
+    ],
   }
 }
